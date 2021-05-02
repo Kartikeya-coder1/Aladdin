e2ee444d92f09c4a2b8dc2cbe8b191ab5228cba0
Kartikeya-coder1 <75845078+Kartikeya-coder1@users.noreply.github.com>
2021-05-02 20:05:14 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !! (answer in CAPITALS)", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 550,150)
+    fill("lightblue")
+    text("Hint: Stores the information for our game !! (answer in CAPITALS)", 550,170)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 300,300)
+    fill("lightblue")
+    text("Hint: Can be <<CALLED>> to perform some tasks !! (answer in CAPITALS)", 300,320)
+
+}
